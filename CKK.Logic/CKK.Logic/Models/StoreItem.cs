f3f686482ab16b63f0b02e5cd9b77d814dad9d68
4400115260 <4400115260@BT144-PROG08>
2022-10-04 11:40:35 -0600

    f

---
+++ CKK.Logic/CKK.Logic/Models/StoreItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    class StoreItem
+    {
+    }
+}
