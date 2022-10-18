92e1529532c6f1dd3c09bff4dd107bc747f06e2b
4400115260 <4400115260@BT144-PROG09>
2022-10-18 14:50:10 -0600

    Added ShoppingCart class

---
+++ CKK.Logic/CKK.Logic/Models/ShoppingCart.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    public class ShoppingCart
+    {
+        private Customer _customer;
+        private Product _product1;
+        private Product _product2;
+        private Product _product3;
+
+        ShoppingCart(Customer customer)
+        {
+            _customer = customer;
+        }
+
+        int GetCustomerId()
+        {
+            return _customer.GetId();
+        }
+
+        ShoppingCartItem GetProductById(int id)
+        {
+            return null;
+        }
+
+        ShoppingCartItem AddProduct(Product product)
+        {
+            return null;
+        }
+
+        ShoppingCartItem AddProduct(Product product, int quantity)
+        {
+            return null;
+        }
+
+        ShoppingCartItem RemoveProduct(Product product, int quantity)
+        {
+            return null;
+        }
+
+        decimal GetTotal()
+        {
+            return 0;
+        }
+
+        ShoppingCart GetProduct(int prodNum)
+        {
+            return null;
+        }
+    }
+}
