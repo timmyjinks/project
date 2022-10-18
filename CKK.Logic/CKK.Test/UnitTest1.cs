f7845499b16db02939e5df0f1501b2cc50e52b72
4400115260 <4400115260@BT144-PROG09>
2022-10-18 13:13:40 -0600

    add test

---
+++ CKK.Logic/CKK.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CKK.Logic.Models;
+
+namespace CKK.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
