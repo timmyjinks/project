2d9fd9ece14f81474dafaaed1ec96e87742dca97
4400115260 <4400115260@BT144-PROG09>
2022-10-18 11:06:44 -0600

    test

---
+++ CKK.Logic/CKK.Tests/CKK.Tests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CKK.Tests;
+
+namespace CKK.Tests
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Customer n = new Customer();
+        }
+    }
+}
---
+++ CKK.Logic/CKKTests/CKK.Tests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CKK.Logic.Models;
+
+namespace CKK.Tests
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void GetSetId_ShouldSetAndReturnCorrectId()
+        {
+            try
+            {
+                Customer c = new Customer(); 
+                
+
+            }
+        }
+    }
+}
